b9352ad5c504b04f615acf0f3b6f8333e69cdae2
carogonzalezp <caro.gonzalezp@hotmail.com>
2024-01-22 11:27:06 -0300

    trabajo sobre el punto 6 de login del sprint 5, creo el archivo profile.ejs, modifico rutas de login y agrego de profile y logout, se crea la carpeta middleware y agrego archivos middlewares,etc

--- The_M_M/src/controllers/usersController.js
+++ The_M_M/src/controllers/usersController.js
@@ -8,6 +8,46 @@ const usersController = {
     login: (req , res) =>{
         res.render('users/login')
     },
+    loginProcess: (req , res) =>{
+        let userToLogin = User.findByField('email, req.body.email');
+       // verifico si la contraseña es la que vino con el req
+        if(userToLogin) {
+            let passwordOk = bcryptjs.compareSync(req.body.password, userToLogin.password);
+            if (passwordOk) {
+                delete userToLogin.password; //esto se hace para no mantener en sesion la info de password del usuario
+                req.session.userLogged = userToLogin;
+                return res.redirect('users/profile');
+            }
+            return res.render('login', {
+                errors: {
+                email: {
+                    msg: 'El mail o contraseña son incorrectos'
+                }
+            }
+            
+        });
+    }
+
+            return res.render('login', {
+                errors: {
+                email: {
+                    msg: 'No se encuentra este mail en nuestra base de datos'
+                }
+            }
+            
+        });
+        
+    },
+    profile: (req , res) =>{
+       return res.render('userProfileLogin', {
+       user: req.session.userLogged
+       });
+    },
+    logout: (req , res) =>{
+         req.session.destroy();
+         return res.redirect('/');
+        
+    },
     adminLogin : (req, res) =>{
         res.render('users/adminLogin')
     },
