cfd6f2bd5446175fd96d6af6b42582fa966a82ff
carogonzalezp <caro.gonzalezp@hotmail.com>
2024-01-28 21:10:08 -0300

    cambios en mi código de login de usersController

--- The_M_M/src/controllers/usersController.js
+++ The_M_M/src/controllers/usersController.js
@@ -39,39 +39,49 @@ const usersController = {
     login: (req , res) =>{
         res.render('users/login');
     },
-    loginProcess: (req , res) =>{
+    loginProcess: (req, res) => {
         let userToLogin = User.findByField('email', req.body.email);
-       // verifico si la contraseña es la que vino con el req
-        if(userToLogin) {
+    
+        if (userToLogin) {
+            // Verificar si req.body.password está definido
+            if (!req.body.password) {
+                return res.render('users/login', {
+                    errors: {
+                        password: {
+                            msg: 'La contraseña es requerida'
+                        }
+                    }
+                });
+            }
+    
             let passwordOk = bcryptjs.compareSync(req.body.password, userToLogin.password);
+    
             if (passwordOk) {
-                delete userToLogin.password; //esto se hace para no mantener en sesion la info de password del usuario
+                delete userToLogin.password;
                 req.session.userLogged = userToLogin;
                 return res.redirect('users/profile');
             }
+    
             return res.render('users/login', {
                 errors: {
+                    email: {
+                        msg: 'El correo o contraseña son incorrectos'
+                    }
+                }
+            });
+        }
+    
+        return res.render('users/login', {
+            errors: {
                 email: {
-                    msg: 'El mail o contraseña son incorrectos'
+                    msg: 'No se encuentra este correo en nuestra base de datos'
                 }
             }
-            
         });
-    }
-
-            return res.render('users/login', {
-                errors: {
-                email: {
-                    msg: 'No se encuentra este mail en nuestra base de datos'
-                }
-            }
-            
-        });
-        
     },
-    profile: (req , res) =>{
+    profile: (req , res) => {
        return res.render('users/userProfileLogin', {
-       user: req.session.userLogged
+       users: req.session.userLogged
        });
     },
     logout: (req , res) =>{
