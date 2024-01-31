ea229d911ca7a20182d08a09af436a3472a32ff1
Fabricio-Porcel <faba_59@hotmail.com>
2024-01-31 14:57:22 +1300

    Arregle el error que habia con las cookies

--- The_M_M/src/middlewares/guestMiddleware.js
+++ The_M_M/src/middlewares/guestMiddleware.js
@@ -1,4 +1,5 @@
 function guestMiddleware(req, res, next) {
+    // console.log("Estado de la sesión:", req.session);
     if(req.session.userLogged){ // si tengo al usuario logeado en sesion quiero redirigirlo a profile
         return res.redirect('/users/profile');
     }
