1b61784a479a16ed98ca5186e658d3debc105d2b
carogonzalezp <caro.gonzalezp@hotmail.com>
2024-03-15 01:59:48 -0300

    me había quedado una modificación del resgister sin guardar

--- The_M_M/public/js/login.js
+++ The_M_M/public/js/login.js
@@ -1,29 +1,32 @@
 window.addEventListener('load', () => {
   let formularioLogin = document.querySelector('#formularioLogin');
+
   formularioLogin.addEventListener("submit", function (e) {
     e.preventDefault();
 
     let errores = [];
+    
     let inputEmail = document.querySelector('#emailLogin');
 
-    if (inputEmail.value.trim() === "") {
-      errores.push('Se requiere indicar un correo de mail');
+    if(inputEmail.value.trim() === ""){
+      errores.push ('Se requiere indicar un correo de mail');
     } else {
       // Validación de formato de correo electrónico
       const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
       if (!emailRegex.test(inputEmail.value)) {
-        errores.push('El formato de correo electrónico no es válido');
+          errores.push('El formato de correo electrónico no es válido');
       }
-
-      let inputContraseña = document.querySelector('#contraseña');
-
-      if (inputContraseña.value.trim() === "") {
-        errores.push('Se requiere completar con la contraseña');
-      } else if (inputContraseña.value.length < 8) {
-        errores.push('la contraseña, debe contener al menos 8 caracteres');
-      }
-
+      
     }
+    
+    let inputContraseña = document.querySelector('#contraseña');
+  
+    if(inputContraseña.value.trim() === ""){
+      errores.push ('Se requiere completar con la contraseña');
+    } else if (inputContraseña.value.length < 8){
+      errores.push ('la contraseña, debe contener al menos 8 caracteres');
+    }
+
     // Mostrar mensajes de error
     let mensajeErrores = document.querySelector('#mensajeErrores');
     if (errores.length > 0) {
--- The_M_M/public/js/register.js
+++ The_M_M/public/js/register.js
@@ -1,75 +1,74 @@
 window.addEventListener('load',() => {
- let formularioRegistro = document.querySelector('#formularioRegistro');
-
- formularioRegistro.addEventListener("submit", function(e){
-  e.preventDefault();
-
-  let errores = [];
-
-  let inputNombre = document.querySelector('#nombre');
-
-  if(inputNombre.value.trim() === ""){
-    errores.push ('Introduzca su nombre');
-  } 
-   if (inputNombre.value.length < 2){
-    errores.push ('el campo de nombre debe tener al menos 2 caracteres');
-  }
-  
-  let inputApellido = document.querySelector('#apellido');
-
-  if(inputApellido.value.trim() === ""){
-    errores.push ('Introduzca su apellido');
-  }
-    if (inputApellido.value.length < 2){
-    errores.push ('el campo de apellido debe tener al menos 2 caracteres');
-  }
-
-  let inputEmail = document.querySelector('#emailRegister');
-
-  if(inputEmail.value.trim() === ""){
-    errores.push ('Se requiere indicar un correo de mail');
-  } else {
-    // Validación de formato de correo electrónico
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(inputEmail.value)) {
-        errores.push('El formato de correo electrónico no es válido');
-    }
-    
-  }
-  
-  let inputContraseña = document.querySelector('#contraseña');
-
-  if(inputContraseña.value.trim() === ""){
-    errores.push ('Se requiere completar con la contraseña');
-  }  else if (inputContraseña.value.length < 8){
-    errores.push ('la contraseña, debe contener al menos 8 caracteres');
-  } else {
-    // Validación de contraseña con mayúsculas, minúsculas, número y carácter especial
-    const contraseñaRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/;
-    if (!contraseñaRegex.test(inputContraseña.value)) {
-        errores.push('La contraseña debe tener letras mayúsculas, minúsculas, un número y un carácter especial');
-    }
-  }
-  // Validación de formato de archivo de imagen 
-  let inputFileImage = document.querySelector('#avatar');
-  if (inputFileImage.value.trim() !== "") {
-      const extensionesPermitidas = /\.(jpg|jpeg|png|gif)$/i;
-      if (!extensionesPermitidas.test(inputFileImage.value)) {
-          errores.push('La imagen debe ser un archivo válido (JPG, JPEG, PNG, GIF)');
-      }
-  }
-
-  
-// Mostrar mensajes de error
-let mensajeErrores = document.querySelector('#mensajeErrores');
-if (errores.length > 0) {
-    mensajeErrores.innerHTML = '';
-    errores.forEach(error => {
-        mensajeErrores.innerHTML += `<li class='text-danger'>${error}</li>`;
-    });
-} else {
-    formularioRegistro.submit(); // Envío del formulario si no hay errores
-}
-});
-}); 
-
+  let formularioRegistro = document.querySelector('#formularioRegistro');
+ 
+  formularioRegistro.addEventListener("submit", function(e){
+   e.preventDefault();
+ 
+   let errores = [];
+ 
+   let inputNombre = document.querySelector('#nombre');
+ 
+   if(inputNombre.value.trim() === ""){
+     errores.push ('Introduzca su nombre');
+   } 
+    if (inputNombre.value.length < 2){
+     errores.push ('el campo de nombre debe tener al menos 2 caracteres');
+   }
+   
+   let inputApellido = document.querySelector('#apellido');
+ 
+   if(inputApellido.value.trim() === ""){
+     errores.push ('Introduzca su apellido');
+   }
+     if (inputApellido.value.length < 2){
+     errores.push ('el campo de apellido debe tener al menos 2 caracteres');
+   }
+ 
+   let inputEmail = document.querySelector('#email');
+ 
+   if(inputEmail.value.trim() === ""){
+     errores.push ('Se requiere indicar un correo de mail');
+   } else {
+     // Validación de formato de correo electrónico
+     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+     if (!emailRegex.test(inputEmail.value)) {
+         errores.push('El formato de correo electrónico no es válido');
+     }
+     
+   }
+   
+   let inputContraseña = document.querySelector('#contraseña');
+ 
+   if(inputContraseña.value.trim() === ""){
+     errores.push ('Se requiere completar con la contraseña');
+   }  else if (inputContraseña.value.length < 8){
+     errores.push ('la contraseña, debe contener al menos 8 caracteres');
+   } else {
+     // Validación de contraseña con mayúsculas, minúsculas, número y carácter especial
+     const contraseñaRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/;
+     if (!contraseñaRegex.test(inputContraseña.value)) {
+         errores.push('La contraseña debe tener letras mayúsculas, minúsculas, un número y un carácter especial');
+     }
+   }
+   // Validación de formato de archivo de imagen 
+   let inputFileImage = document.querySelector('#avatar');
+   if (inputFileImage.value.trim() !== "") {
+       const extensionesPermitidas = /\.(jpg|jpeg|png|gif)$/i;
+       if (!extensionesPermitidas.test(inputFileImage.value)) {
+           errores.push('La imagen debe ser un archivo válido (JPG, JPEG, PNG, GIF)');
+       }
+   }
+ 
+   
+ // Mostrar mensajes de error
+ let mensajeErrores = document.querySelector('#mensajeErrores');
+ if (errores.length > 0) {
+     mensajeErrores.innerHTML = '';
+     errores.forEach(error => {
+         mensajeErrores.innerHTML += `<li class='text-danger'>${error}</li>`;
+     });
+ } else {
+     formularioRegistro.submit(); // Envío del formulario si no hay errores
+ }
+ });
+ }); 
